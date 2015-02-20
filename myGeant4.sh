7bc99cbd3f3b006dfc627e08fe231fecbce71648
NTrevisa <n.trevisani@campus.unimib.it>
2015-02-20 13:09:10 +0100

    source for geant 10.0

--- myGeant4.sh
+++ myGeant4.sh
@@ -1,4 +1,4 @@
-cd /afs/cern.ch/sw/lcg/external/geant4/9.6.p03/x86_64-slc6-gcc46-opt/bin/
+cd /afs/cern.ch/sw/lcg/external/geant4/10.0.p01/x86_64-slc6-gcc47-opt/bin
 #cd /afs/cern.ch/sw/lcg/external/geant4/9.6.p04/x86_64-slc6-gcc46-opt/bin
 source ./geant4.sh
 source ./geant4.sh
